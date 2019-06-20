3a610fae3b06de2e8ca10c863d14d22a4e1b3819
JDB0rg <filthyfeather@gmail.com>
2019-06-19 22:12:10 -0600

    Close to finishing day two code

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -1,4 +1,7 @@
 #include "cpu.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 #define DATA_LEN 6
 
@@ -24,6 +27,19 @@ void cpu_load(struct cpu *cpu)
   }
 
   // TODO: Replace this with something less hard-coded
+  // Implement the cpu_load() function to load an .ls8 file 
+  // Filename should be passed in as an argument
+  //Implement a Multiply instruction and Print the result (run mult8.ls8)
+}
+
+unsigned char cpu_ram_read(struct cpu *cpu, unsigned char mar)
+{
+  return cpu->ram[mar];
+}
+
+void cpu_ram_write(struct cpu *cpu, unsigned char mar, unsigned char mdr)
+{
+  cpu->ram[mar] = mdr;
 }
 
 /**
@@ -47,14 +63,60 @@ void cpu_run(struct cpu *cpu)
 {
   int running = 1; // True until we get a HLT instruction
 
+  unsigned char IR; 
+  unsigned char operandA;
+  unsigned char operandB;
+
   while (running) {
+
     // TODO
     // 1. Get the value of the current instruction (in address PC).
+    IR = cpu_ram_read(cpu, cpu->PC);
     // 2. Figure out how many operands this next instruction requires
+    int next_pc = (IR >> 6); 
+
+    if (next_pc == 2) 
+    { 
+      operandA = cpu_ram_read(cpu, (cpu->PC+1) & 0xff);
+      operandB = cpu_ram_read(cpu, (cpu->PC+2) & 0xff);
+    }
     // 3. Get the appropriate value(s) of the operands following this instruction
+    else if (next_pc == 1) 
+    {
+      operandA = cpu_ram_read(cpu, (cpu->PC+1) & 0xff);
+    }
+    else 
+    {
+      return (1);
+    }
     // 4. switch() over it to decide on a course of action.
+    int instruction_set_pc = (IR >> 4) & 1;
+
+    switch(IR) 
+    {
+      case HLT:
+			  running = 0;
+				break;
+
+      case LDI: 
+        cpu->reg[operandA] = operandB; 
+        break;
+
+      case PRN:
+        printf("%d\n", cpu->reg[operandA]);
+        break;
+      
+      default:
+        printf("unexpected instruction 0x%02x at 0x%02x\n", IR, cpu->PC);
+        exit(1);
+    }
+
     // 5. Do whatever the instruction should do according to the spec.
     // 6. Move the PC to the next instruction.
+    if (!instruction_set_pc)
+    {
+      cpu->PC = cpu->PC + next_pc + 1;
+    }
   }
 }
 
@@ -64,4 +126,12 @@ void cpu_run(struct cpu *cpu)
 void cpu_init(struct cpu *cpu)
 {
   // TODO: Initialize the PC and other special registers
+  
+  //cpu = malloc(sizeof(struct cpu));
+  cpu->PC = 0;
+  // The SP points at address `F4` if the stack is empty.
+  cpu->reg[SP] = 0xF4;
+  cpu->FL = 4;
+  memset(cpu->reg, 0, sizeof(cpu->reg));
+  memset(cpu->ram, 0, sizeof(cpu->ram));
 }
--- ls8/cpu.h
+++ ls8/cpu.h
@@ -3,10 +3,12 @@
 
 // Holds all information about the CPU
 struct cpu {
+  
   // TODO
-  // PC
-  // registers (array)
-  // ram (array)
+  unsigned int PC;        // PC
+  unsigned char reg[8];   // registers (array)
+  unsigned char ram[256]; // ram (array)
+
 };
 
 // ALU operations
@@ -17,11 +19,17 @@ enum alu_op {
 
 // Instructions
 
-// These use binary literals. If these aren't available with your compiler, hex
+// These use binary literals. 
+// If these aren't available with your compiler, hex
 // literals should be used.
 
 #define LDI  0b10000010
 #define PRN  0b01000111
+#define HLT  0b00000001
+#define MUL  0b10100010
+#define PUSH 0b01000101
+#define POP  0b01000110
+
 // TODO: more instructions here. These can be used in cpu_run().
 
 // Function declarations
@@ -29,5 +37,7 @@ enum alu_op {
 extern void cpu_load(struct cpu *cpu);
 extern void cpu_init(struct cpu *cpu);
 extern void cpu_run(struct cpu *cpu);
+extern unsigned char cpu_ram_read(struct cpu *cpu,unsigned char mar);
+extern void cpu_ram_write(struct cpu *cpu, unsigned char mar, unsigned char mdr);
 
 #endif
